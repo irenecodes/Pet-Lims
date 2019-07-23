a17a806e4b72eff171ac419e469ea823d3314d78
Irene Truong <irenesis123@gmail.com>
2019-07-23 03:14:56 -0400

    fixed margin for h3

--- scripts.js
+++ scripts.js
@@ -4,32 +4,32 @@ const petsByCity = {
     Toronto: {
         cat: 6,
         dog: 3,
-        either: `3 dogs and 6 cats`,
-        bylaw: `<a href="https://www.toronto.ca/311/knowledgebase/kb/docs/articles/municipal-licensing-and-standards/toronto-animal-services/maximum-number-of-pets-dogs-cats-allowed-in-a-home.html" target="_blank">City of Toronto Municipal Code, Chapter 349</a>`
+        either: '3 dogs and 6 cats',
+        bylaw: '<a href="https://www.toronto.ca/311/knowledgebase/kb/docs/articles/municipal-licensing-and-standards/toronto-animal-services/maximum-number-of-pets-dogs-cats-allowed-in-a-home.html" target="_blank">City of Toronto Municipal Code, Chapter 349</a>'
     },
     Vaughan: {
         cat: 3,
         dog: 3,
-        either: `3 dogs and 3 cats`,
-        bylaw: `<a href="https://www.vaughan.ca/services/residential/animal_services/Pages/animal_bylaws.aspx" target="_blank">Animal Control By-law 53-2002</a>`
+        either: '3 dogs and 3 cats',
+        bylaw: '<a href="https://www.vaughan.ca/services/residential/animal_services/Pages/animal_bylaws.aspx" target="_blank">Animal Control By-law 53-2002</a>'
     },
     RichmondHill: {
         cat: 6,
         dog: 4,
-        either: `4 dogs and 6 cats`,
-        bylaw: `<a href="https://www.richmondhill.ca/en/our-services/Animal-Services.aspx" target="_blank">Richmond Hill By-law Chapter 309 and Chapter 314</a>`
+        either: '4 dogs and 6 cats',
+        bylaw: '<a href="https://www.richmondhill.ca/en/our-services/Animal-Services.aspx" target="_blank">Richmond Hill By-law Chapter 309 and Chapter 314</a>'
     },
     Markham: {
         cat: 4,
         dog: 2,
-        either: `4 pets in any of the following ways: 2 dogs and 2 cats, 1 dog and 3 cats, or 4 cats`,
-        bylaw: `<a href="https://www.markham.ca/wps/portal/home/about/city-hall/bylaws/bylaw-guide-for-homeowners/animal-control" target="_blank">Animal Control By-law 2018-91</a>`
+        either: '4 pets in any of the following ways: 2 dogs and 2 cats, 1 dog and 3 cats, or 4 cats',
+        bylaw: '<a href="https://www.markham.ca/wps/portal/home/about/city-hall/bylaws/bylaw-guide-for-homeowners/animal-control" target="_blank">Animal Control By-law 2018-91</a>'
     },
     Brampton: {
         cat: 6,
         dog: 3,
-        either: `3 dogs and 6 cats`,
-        bylaw: `<a href="http://www.brampton.ca/en/City-Hall/Bylaws/Pages/Animal-By-laws.aspx" target="_blank">Animal by-laws 250-2005 and 261-93</a>`
+        either: '3 dogs and 6 cats',
+        bylaw: '<a href="http://www.brampton.ca/en/City-Hall/Bylaws/Pages/Animal-By-laws.aspx" target="_blank">Animal by-laws 250-2005 and 261-93</a>'
     }
 }
 
@@ -38,7 +38,7 @@ $(function () {
     //buttons activate once clicked and allow user to only choose one option per fieldset - inspired by/credit to https://teamtreehouse.com/community/removing-class-when-another-div-is-clicked
     $('label').on('click', function () {
         $(this).siblings().removeClass(`check`);
-        $(this).toggleClass('check');
+        $(this).addClass('check');
     });
 
     // creating an event for when user choices are made
